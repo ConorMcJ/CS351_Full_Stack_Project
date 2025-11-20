243c23895d3660591970d1c6aa27de7f532908a5
OminousRose <eric.flores.cs@outlook.com>
2025-11-20 16:54:54 -0600

    Remove MSW from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App.jsx';
 
-async function prepare() {
-  if (import.meta.env.DEV) {
-    const { worker } = await import("./mocks/browser");
-    await worker.start();
-  }
-}
-
   ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
       <BrowserRouter>
