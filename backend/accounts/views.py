4db8baccfde1becd21c6aeb521b3301d0ab09205
Sammy Ammra <sammr@uic.edu>
2025-11-19 20:28:22 -0600

    backend update

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -6,17 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .models import UserProfile
 from .serializers import UserDetailSerializer, UserProfileSerializer
-import json
 
-from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.contrib.auth.tokens import default_token_generator
-from django.http import JsonResponse, HttpResponseBadRequest
-from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
-from django.views.decorators.csrf import csrf_exempt
-from django.core.mail import send_mail
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
